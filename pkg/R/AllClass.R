21797c0c008dd9a219e11dbb113221314f6009b8
yusung <yusung@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-19 14:34:04 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2jags@85 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AllClass.R
+++ pkg/R/AllClass.R
@@ -1,9 +1,142 @@
-methods::setOldClass("jags")
-methods::setOldClass("bugs")
-methods::setOldClass("mcmc.list")
+# ==============================================================================
+# class definition 
+# ==============================================================================
+methods::setOldClass("lm")
+methods::setOldClass("glm")
 
-methods::setClass("rjags",
-     representation(
-            model = "jags",
-            BUGSoutput = "bugs")
+setClass("mi.info")
+        
+
+
+setClass("mi",
+            representation(
+                call      = "call",
+                data      = "data.frame" ,
+                m         = "numeric",
+                mi.info   = "mi.info",
+                imp       = "list",
+                mcmc      = "ANY",
+                converged = "logical",
+                coef.mcmc      = "ANY",
+                coef.converged = "logical",
+                add.noise = "logical",
+                total.iters  = "numeric"),
+            contains  = "list"
+)
+
+
+
+
+
+#setClass("mi.initialize",
+#            representation(
+#              data          = "data.frame",
+#              mi.data       = "list",
+#              VarName       = "vector",
+#              start.val     = "list",
+#              coef.val      = "list",
+#              mi.object     = "list",
+#              length.list   = "list",
+#              col.mis       = "logical",
+#              ncol.mis      = "numeric",
+#              iteration.idx = "integer"),            
+#              contains  = "list"
+#)
+
+
+
+setClass("mi.method",
+        representation(
+          model     = "list",
+          expected  = "ANY",
+          random    = "ANY"),
+          contains  = "list"
+)
+
+
+setClass("mi.continuous",
+        representation(
+            model    = "list", 
+            expected = "numeric", 
+            random   = "ANY"),
+            contains = "mi.method"
+)
+
+setClass("mi.count",
+        representation(
+            model    = "list", 
+            expected = "numeric", 
+            random   = "ANY"),
+            contains = "mi.method"
+)
+
+
+
+
+setClass("mi.binary",
+        representation(
+            model    = "list", 
+            expected = "ANY", 
+            random   = "ANY"),
+            contains = "mi.method"
+)
+
+setClass("mi.categorical",
+        representation(
+            model     = "list", 
+            expected  = "ANY", 
+            random    = "ANY"),
+            contains  = "mi.method"
+)
+
+setClass("mi.polr",
+        representation(
+            model     = "list", 
+            expected  = "ANY", 
+            random    = "ANY"),
+            contains  = "mi.method"
+)
+
+
+setClass("mi.fixed",
+        representation(
+            model    = "list", 
+            expected = "ANY", 
+            random   = "ANY"),
+            contains = "mi.method"
+)
+
+
+setClass("mi.pmm",
+        representation(
+            model     = "list", 
+            expected  = "ANY", 
+            random    = "ANY",
+            residuals = "numeric"),
+            contains  = "mi.method"
+)
+
+setClass("mi.copy",
+        representation(
+            model    = "list", 
+            expected = "ANY", 
+            random   = "ANY"),
+            contains = "mi.method"
+)
+
+
+setClass("mi.pooled",
+            representation(
+                call       = "call",
+                mi.pooled  = "list" ,
+                mi.fit     = "list"),
+                contains   = "list"
+)
+
+
+setClass("mi.preprocessed",
+            representation(
+                data      = "data.frame" ,
+                mi.info   = "mi.info"),
+            contains  = "list"
 )
